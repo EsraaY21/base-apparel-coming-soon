a134df77538cdcc7c1ffac2c708c34f109a5d8af
Esra <esraayaareb@gmail.com>
2024-12-16 02:01:14 +0300

    desktop design

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
+import IconArrow from "./components/icons/IconArrow";
+
 function App() {
   return (
     <>
       <main>
-        <div className="text-container">
-          <img src="" alt="logo" />
-          <h1>We're coming soon</h1>
-          <p>
-            Hello fellow shoppers! We're currently building our new fashion
-            store. Add your email below to stay up-to-date with announcements
-            and our launch deals.
-          </p>
-          <form>
-            <input type="text" placeholder="Email Address" />
-            <button type="submit"></button>
-          </form>
+        <div className="left">
+          <div className="text-container">
+            <img
+              src="./resources/images/logo.svg"
+              alt="Logo"
+              className="logo"
+            />
+            <h1>
+              <span>We're</span>
+              <br />
+              coming soon
+            </h1>
+            <p>
+              Hello fellow shoppers! We're currently building our new fashion
+              store. Add your email below to stay up-to-date with announcements
+              and our launch deals.
+            </p>
+            <form>
+              <input type="text" placeholder="Email Address" />
+              <button type="submit">
+                <IconArrow />
+              </button>
+            </form>
+          </div>
         </div>
-        {/* <img src="./resources/" alt="" /> */}
+
+        <img
+          src="./resources/images/hero-desktop.jpg"
+          alt="Cover"
+          className="cover-img"
+        />
       </main>
     </>
   );
