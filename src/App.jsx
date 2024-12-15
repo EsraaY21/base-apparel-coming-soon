0d8c69e4de1280637b801fcdeb7259cb1ddca5f7
Esra <esraayaareb@gmail.com>
2024-12-15 23:38:55 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <>
+      <main>
+        <div className="text-container">
+          <img src="" alt="logo" />
+          <h1>We're coming soon</h1>
+          <p>
+            Hello fellow shoppers! We're currently building our new fashion
+            store. Add your email below to stay up-to-date with announcements
+            and our launch deals.
+          </p>
+          <form>
+            <input type="text" placeholder="Email Address" />
+            <button type="submit"></button>
+          </form>
+        </div>
+        {/* <img src="./resources/" alt="" /> */}
+      </main>
+    </>
+  );
+}
+
+export default App;
