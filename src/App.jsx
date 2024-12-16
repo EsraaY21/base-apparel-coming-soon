c0115d374d1635585f4e90a7c04cf0c896171a5e
Esra <esraayaareb@gmail.com>
2024-12-16 03:30:25 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -28,42 +28,50 @@ function App() {
               alt="Logo"
               className="logo"
             />
-            <h1>
-              <span>We're</span>
-              <br />
-              coming soon
-            </h1>
-            <p>
-              Hello fellow shoppers! We're currently building our new fashion
-              store. Add your email below to stay up-to-date with announcements
-              and our launch deals.
-            </p>
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                placeholder="Email Address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className={error ? "error" : ""}
-              />
-              {error && (
-                <span className="error-icon">
-                  <IconError />
-                </span>
-              )}
 
-              <button type="submit">
-                <IconArrow />
-              </button>
-              {error && <p className="error">{error}</p>}
-            </form>
+            <img
+              src="./resources/images/hero-mobile.jpg"
+              alt="Cover"
+              className="cover-mobile"
+            />
+            <div className="container">
+              <h1>
+                <span>We're</span>
+                <br />
+                coming soon
+              </h1>
+              <p>
+                Hello fellow shoppers! We're currently building our new fashion
+                store. Add your email below to stay up-to-date with
+                announcements and our launch deals.
+              </p>
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  placeholder="Email Address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className={error ? "error" : ""}
+                />
+                {error && (
+                  <span className="error-icon">
+                    <IconError />
+                  </span>
+                )}
+
+                <button type="submit">
+                  <IconArrow />
+                </button>
+                {error && <p className="error">{error}</p>}
+              </form>
+            </div>
           </div>
         </div>
 
         <img
           src="./resources/images/hero-desktop.jpg"
           alt="Cover"
-          className="cover-img"
+          className="cover-desktop"
         />
       </main>
     </>
